13764d9dcd8ace2c0203095a9b0960447482f99e
Lasha Krikheli <lkrikheli@candy.com>
2025-04-09 22:23:22 -0400

    Remove todo for image optimization in next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: false, // TODO: change this to false to enable Next's image optimization
+    unoptimized: false,
     domains: ["picsum.photos", "api.air.inc", "air-prod.imgix.net"],
   },
   experimental: {
